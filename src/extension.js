722f5370947176c164b57dd14f2209ffe6e7dba8
Fuzail <fuzailgilani@gmail.com>
2018-04-24 18:43:49 -0500

    added helper functions to create pixel for tracking emails, TODO test code

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,28 @@
 "use strict";
 
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+
+function getTrackingGIF(account, email, subject) {
+
+  var imgURL = "https://www.google-analytics.com/collect?"
+    + "v=1&t=event"
+    + "&tid=" + account
+    + "&z="   + Math.round((new Date()).getTime()/1000).toString()
+    + "&cid=" + guid()
+    + "&ec=email"
+    + "&ea=open"
+    + "&el=" + encodeURIComponent("sent to: " + email + "; subject: " + subject);
+
+  return "<img src='" + imgURL + "' width='1' height='1'/>";
+
+}
+
 console.log("Extension loading...");
 const jQuery = require("jquery");
 const $ = jQuery;
