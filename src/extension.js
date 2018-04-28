75ec363338de700e7fb03c1465cfe828b6a08bd5
Fuzail <fuzailgilani@gmail.com>
2018-04-28 13:33:12 -0500

    finalized replacement of hyperlinks. Extension fulfills all requirements (will get advice regarding Google Analytics). Added comments and organized code

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,10 @@
 "use strict";
 
+// guid()
+// randomly generates pseudo-GUID for use in Google Analytics event
+//
+// input: N/A
+// output: String containing randomly generated GUID
 function guid() {
   function s4() {
     return Math.floor((1 + Math.random()) * 0x10000)
@@ -8,11 +13,48 @@ function guid() {
   }
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
-function getTrackingGIF(account, eventDetails) {
+
+// swapHyperLinks()
+// parses through HTML string passed through parameter to find href elements and swap hyperlinks to go through proxy.playposit.com
+//
+// input: html - string containing HTML to be parsed
+// output: String containing modified HTML from parameter
+function swapHyperLinks(html) {
+
+  //regex object to be used for non-SSL hyperlinks
+  var regex = /href\s*=\s*(['"])(http:\/\/)(.+?)\1/ig;
+
+  //regex object to be used for SSL hyperlinks
+  var regexSSL = /href\s*=\s*(['"])(https:\/\/)(.+?)\1/ig;
+  var link;
+
+
+  //it is important that the SSL hyperlinks are replaced first
+  //non-SSL hyperlinks will be replaced with an SSL hyperlink to playposit
+  //if non-SSL are replaced first, then the SSL regex will replace the modified non-SSL links as well
+  while((link = regexSSL.exec(html)) !== null) {
+    console.log(link);
+    html = html.replace(link[3], "https://proxy.playposit.com/ssl/" + encodeURIComponent(link[3]));
+  }
+  while((link = regex.exec(html)) !== null) {
+    console.log(link);
+    html = html.replace(link[3], "https://proxy.playposit.com/http/" + encodeURIComponent(link[3]));
+  }
+
+  return html;
+}
+
+// getTrackingGIF()
+// uses info passed through parameters to construct a 1x1 pixel to be appended onto an email for tracking email opens
+//
+// input: trackerID - Google Analytics tracker ID
+//        eventDetails - event label for GA event, should contain details about sender, recipients, subject, and date sent
+// output: HTML defining image element containing tracking GIF for Google Analytics
+function getTrackingGIF(trackerID, eventDetails) {
 
   var imgURL = "https://ssl.google-analytics.com/collect?"
     + "v=1"
-    + "&tid=" + account
+    + "&tid=" + trackerID
     + "&cid=" + guid()
     + "&t=event"
     + "&ec=email"
@@ -23,6 +65,7 @@ function getTrackingGIF(account, eventDetails) {
 
 }
 
+//set up extension, load required packages
 console.log("Extension loading...");
 const jQuery = require("jquery");
 const $ = jQuery;
@@ -30,27 +73,27 @@ const GmailFactory = require("gmail-js");
 const gmail = new GmailFactory.Gmail($);
 window.gmail = gmail;
 
+//wait for full Gmail interface to load before running extension
 gmail.observe.on("load", () => {
-    const userEmail = gmail.get.user_email();
-    console.log("Hello, " + userEmail + ". This is your extension talking!");
-    let list = gmail.get.visible_emails();
-    console.log(list.length);
-    console.log(getTrackingGIF("UA-118160763-1","test"));
 
+    const userEmail = gmail.get.user_email();
+
+    //when user presses send message button, parse and modify email to add tracking gif and replace hyperlinks
     gmail.observe.before('send_message', function(url, body, data, xhr){
+      //collect email body data from XHR
       var body_params = xhr.xhrParams.body_params;
 
       //construct event label for Google Analytics event
       let timeSent = new Date().toDateString();
-      let eventDetails = "Sent by: " + gmail.get.user_email() + "Subject: " + body_params.subject + " Time sent: " + timeSent + " Email recipients: " + body_params.to;
-      console.log(eventDetails);
+      let eventDetails = "Sent by: " + userEmail + "Subject: " + body_params.subject + " Time sent: " + timeSent + " Email recipients: " + body_params.to;
 
       //create HTML for tracking GIF to be appended to email body
       let gifHTML = getTrackingGIF("UA-118160763-1", eventDetails);
-      console.log(gifHTML);
+
+      //replace hyperlinks with proxy.playposit.com redirects
+      body_params.body = swapHyperLinks(body_params.body);
 
       //append tracking GIF to email before sending
       body_params.body += gifHTML;
-      console.log("sending message, url:", url, 'body', body, 'email_data', data, 'xhr', xhr);
     });
 });
